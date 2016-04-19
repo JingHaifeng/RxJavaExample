c48979c9c5407534ed4e463c60d56a5aee32d08d
景海峰 <haifeng_jing@kingdee.com>
2016-04-19 11:17:06 +0800

    init

---
+++ app/src/main/java/com/jinghaifeng/rxjavaexample/OperatorFragment.java
@@ -0,0 +1,12 @@
+package com.jinghaifeng.rxjavaexample;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author: haifeng jing(haifeng_jing@kingdee.com)
+ * @date: 2016-04-19
+ * @time: 10:24
+ */
+public class OperatorFragment extends Fragment {
+
+}
